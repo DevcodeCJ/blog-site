b49b4443051c265a355763ca0150baa202d9f103
CJ <chiji.davids@yahoo.com>
2021-04-06 07:09:44 -0600

    adding files

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes.js");
+
+// Connect to MongoDB
+const dbURI =
+  "mongodb+srv://mau:test1234@node-blogsite.wfqaq.mongodb.net/Node-BlogSite?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((response) => {
+    // Launch Express App once connected to db
+    app.listen(3000);
+    console.log("Connected to the database");
+  })
+  .catch((err) => console.log(err));
+
+// Register View Engine
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(express.static("public"));
+// Access to Form Data
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+// app.use((req, res, next) => {
+//   console.log("New request made:");
+//   console.log("Host: ", req.hostname);
+//   console.log("Path: ", req.path);
+//   console.log("Method: ", req.method);
+//   next();
+// });
+
+// Mongoose and Mongo Sandbox Routes
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "New Blog 3",
+//     snippet: "About my new blog",
+//     body: "More about my new blog",
+//   });
+//   blog
+//     .save()
+//     .then((response) => {
+//       res.send(response);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((response) => {
+//       res.send(response);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("6069c67b231f6c05bfbf8159")
+//     .then((response) => {
+//       res.send(response);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// Listen for Requests | Routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+  // const blogs = [
+  //   {
+  //     title: "Yoshi finds eggs",
+  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
+  //   },
+  //   {
+  //     title: "Mario finds stars",
+  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
+  //   },
+  //   {
+  //     title: "How to defeat Bowser",
+  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
+  //   },
+  // ];
+  // res.render("index", { title: "Home", blogs });
+  // res.status(200).sendFile("./views/index.html", { root: __dirname });
+  // res.send("<h1>Home Page</h1>");
+  // console.log(__dirname);
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+  // res.status(200).sendFile("./views/about.html", { root: __dirname });
+});
+
+// Blog Routes
+app.use(blogRoutes);
+
+// Redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 Page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+  // res.status(404).sendFile("/views/404.html", { root: __dirname });
+});
---
+++ controllers/blogController.js
@@ -0,0 +1,62 @@
+const Blog = require("../models/blog");
+// NAMING CONVENTION --> blog_index, blog_create_post, blog_create_get, blog_details, blog_delete
+
+const blog_index = (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((response) => {
+      res.render("index", { title: "All Blogs", blogs: response });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const blog_create_post = (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((response) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const blog_create_get = (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+};
+
+const blog_details = (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((response) => {
+      res.render("details", { blog: response, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const blog_delete = (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((response) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  blog_index,
+  blog_create_post,
+  blog_create_get,
+  blog_details,
+  blog_delete,
+};
