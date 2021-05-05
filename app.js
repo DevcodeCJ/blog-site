bdcaac8265049eba71faf47c3671324caa976480
CJ <chiji.davids@yahoo.com>
2021-05-05 07:18:22 -0600

    updated app.js and routes

--- app.js
+++ app.js
@@ -23,89 +23,11 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 // app.use(morgan("dev"));
 
-// app.use((req, res, next) => {
-//   console.log("New request made:");
-//   console.log("Host: ", req.hostname);
-//   console.log("Path: ", req.path);
-//   console.log("Method: ", req.method);
-//   next();
-// });
-
-// Mongoose and Mongo Sandbox Routes
-// app.get("/add-blog", (req, res) => {
-//   const blog = new Blog({
-//     title: "New Blog 3",
-//     snippet: "About my new blog",
-//     body: "More about my new blog",
-//   });
-//   blog
-//     .save()
-//     .then((response) => {
-//       res.send(response);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/all-blogs", (req, res) => {
-//   Blog.find()
-//     .then((response) => {
-//       res.send(response);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/single-blog", (req, res) => {
-//   Blog.findById("6069c67b231f6c05bfbf8159")
-//     .then((response) => {
-//       res.send(response);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// Listen for Requests | Routes
-app.get("/", (req, res) => {
-  res.redirect("/blogs");
-  // const blogs = [
-  //   {
-  //     title: "Yoshi finds eggs",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
-  //   },
-  //   {
-  //     title: "Mario finds stars",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
-  //   },
-  //   {
-  //     title: "How to defeat Bowser",
-  //     snippet: "Lorem ipsum dolor sit amet consectetur adipisicing",
-  //   },
-  // ];
-  // res.render("index", { title: "Home", blogs });
-  // res.status(200).sendFile("./views/index.html", { root: __dirname });
-  // res.send("<h1>Home Page</h1>");
-  // console.log(__dirname);
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
-  // res.status(200).sendFile("./views/about.html", { root: __dirname });
-});
-
-// Redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+// Register View Engine
+app.set("view engine", "ejs");
 
 // Blog Routes
 app.use("/", blogRoutes);
-
-// Register View Engine
-app.set("view engine", "ejs");
 
 // Access to Static Files
 app.use(express.static("public"));
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blogController.js");
 
+router.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+  // res.status(200).sendFile("./views/about.html", { root: __dirname });
+});
+
 router.get("/blogs", blogController.blog_index);
 
 router.post("/blogs", blogController.blog_create_post);
