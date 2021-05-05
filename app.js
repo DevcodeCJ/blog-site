ce579db99b71e8666784fc7c0870216535b4c2fd
CJ <chiji.davids@yahoo.com>
2021-05-05 06:59:41 -0600

    updated app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Listen for Requests | Routes
 app.get("/", (req, res) => {
-  res.redirect("/about");
+  res.render("index");
   // const blogs = [
   //   {
   //     title: "Yoshi finds eggs",
